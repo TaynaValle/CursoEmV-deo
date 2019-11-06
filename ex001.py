75b981c20cdec7b8055a95086f3fc249ef4e1d2f
TaynaValle <56053319+TaynaValle@users.noreply.github.com>
2019-11-06 01:16:57 -0300

    Add files via upload

---
+++ ex001.py
@@ -0,0 +1,2 @@
+"""Crie um programa de leie olá mundo na tela """
+print('Olá mundo!')
---
+++ ex003.py
@@ -0,0 +1,5 @@
+"""Crie um programa que mostre a soma entre dois números e mostre na tela"""
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite outro número:'))
+soma = n1 + n2
+print('A soma entre {} mais {} é igual a {}'.format(n1, n2, soma))
