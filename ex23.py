4e02ce92206c2f7b888c8e82098b245640a81060
TaynaValle <56053319+TaynaValle@users.noreply.github.com>
2022-07-12 14:15:21 -0300

    Create ex23.py

---
+++ ex23.py
@@ -0,0 +1,18 @@
+##Faça um programa que leia um número de 0 à 9999 e mostre na tela ca um dos dígitos separados
+##Ex:Digite um número:1834
+##unidade:4
+##dezena:8
+##centena:3
+##Milhar:1
+
+numero = int(input('Digite um número de 0 à 9999: '))
+u = numero//1%10
+d = numero//10%10
+c = numero//100%10
+m = numero//1000%10
+print('Analisando o número {}'.format(numero))
+
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
